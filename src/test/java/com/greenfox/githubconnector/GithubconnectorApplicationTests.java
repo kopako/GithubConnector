ce7b788154968db2eee5b2ea8db48e4bee2486d0
Kopan Dmytro <kopandima@gmail.com>
2019-04-02 13:19:02 +0200

    init(githubconnector): Initial commit SpringBoot

---
+++ src/test/java/com/greenfox/githubconnector/GithubconnectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfox.githubconnector;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubconnectorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
